fbe7d9e6c83f20c44e6e8c2e78d0bd46ccd17df3
Jussi Niskanen <jussi.niskanen@gofore.com>
2014-01-09 15:05:35 +0200

    Opintoalan valinta lisää rajapintapyyntöö 'opintoala' -parametrin

--- eperusteet/eperusteet-app/yo/app/scripts/controllers/haku.js
+++ eperusteet/eperusteet-app/yo/app/scripts/controllers/haku.js
@@ -18,7 +18,9 @@ angular.module('eperusteApp')
     $scope.kontekstit = YleinenData.kontekstit;
     $scope.kieli = YleinenData.kieli;
     $scope.koulutusalat = Koulutusalat.query();
-    $scope.opintoalat = []
+    $scope.opintoalat = [];
+    $scope.valittuOpintoala = Haku.hakuParametrit.opintoala;
+    
     
     $scope.tutkintotyypit = {
       1: 'tutkintotyyppikoodi-1',
@@ -83,6 +85,7 @@ angular.module('eperusteApp')
         $scope.tutkintotyyppi = '';
       }
     };
+    
     if ($routeParams.konteksti && $scope.kontekstit.indexOf($routeParams.konteksti.toLowerCase()) !== -1) {
       $scope.konteksti = $routeParams.konteksti;
       alustaTutkintotyyppi();
@@ -93,6 +96,7 @@ angular.module('eperusteApp')
     $scope.tyhjenna = function() {
       $scope.query = null;
       $scope.koulutusala = '';
+      $scope.valittuOpintoala = '';
       alustaTutkintotyyppi();
       $scope.nykyinenSivu = 0;
       $scope.haePerusteet(0);
@@ -111,6 +115,7 @@ angular.module('eperusteApp')
         sivu: sivu,
         nimi: $scope.query,
         ala: $scope.koulutusala,
+        opintoala: $scope.valittuOpintoala,
         sivukoko: $scope.sivukoko,
         tyyppi: $scope.tutkintotyyppi,
         kieli: YleinenData.kieli
@@ -150,7 +155,7 @@ angular.module('eperusteApp')
       $scope.tyhjenna();
       $scope.haePerusteet(0);
     });
-    
+       
     $scope.koulutusalaMuuttui = function() {
       if ($scope.koulutusala !== '') {
         $scope.opintoalat = $scope.koulutusalat[parseInt($scope.koulutusala) -1].opintoalat;        
