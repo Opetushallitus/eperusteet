5b51c9035cf287a86698bbc872d93957e6582c60
Jussi Niskanen <jussi.niskanen@gofore.com>
2014-01-09 11:37:04 +0200

    Lisätty tiedostot koulutusalan rajapintaa varten

---
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/resource/KoulutusalaController.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2013 The Finnish Board of Education - Opetushallitus
+ * 
+ * This program is free software: Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ * 
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * European Union Public Licence for more details.
+ */
+
+package fi.vm.sade.eperusteet.resource;
+
+import fi.vm.sade.eperusteet.domain.Koulutusala;
+import fi.vm.sade.eperusteet.service.KoulutusalaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author jussini
+ */
+@Controller
+@RequestMapping("/api/koulutusala")
+public class KoulutusalaController {
+        
+    @Autowired
+    private KoulutusalaService service;
+    
+    @RequestMapping(method = GET)
+    @ResponseBody
+    public List<Koulutusala> getAll() {
+        List<Koulutusala> klist = service.getAll();
+        return klist;       
+    }
+   
+    @RequestMapping(value = "/{id}", method = GET)
+    @ResponseBody
+    public ResponseEntity<Koulutusala> get(@PathVariable("id") final Long id) {               
+        Koulutusala k = service.get(id);
+        if (k == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(k, HttpStatus.OK);
+    }
+       
+}
+
