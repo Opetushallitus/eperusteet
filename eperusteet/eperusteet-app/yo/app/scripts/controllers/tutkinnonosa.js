b1d3739cbb67d22aaa897208d5620e6036efad58
Harri Kemppi <harri.kemppi@gofore.com>
2014-01-27 15:20:05 +0200

    Tutkinnon osan data haetaan backendiltä. Tutkinnon osan näyttämiseen oma sivu.

--- eperusteet/eperusteet-app/yo/app/scripts/controllers/tutkinnonosa.js
+++ eperusteet/eperusteet-app/yo/app/scripts/controllers/tutkinnonosa.js
@@ -1,10 +1,41 @@
 'use strict';
 
 angular.module('eperusteApp')
-  .controller('TutkinnonosaCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('TutkinnonosaCtrl', function ($scope, $routeParams, $location,
+    YleinenData, PerusteenOsat, Arviointi) {
+    
+    $scope.kontekstit = YleinenData.kontekstit;
+    $scope.arviointi = {};
+  
+    if ($routeParams.konteksti && $scope.kontekstit.indexOf($routeParams.konteksti.toLowerCase()) !== -1) {
+      $scope.konteksti = $routeParams.konteksti;
+    } else {
+      $location.path('/selaus/ammatillinenperuskoulutus');
+    }
+    
+    PerusteenOsat.get({osanId: $routeParams.tutkinnonOsaId}, function(tulos) {
+      
+      console.log('Success1');
+
+        if (tulos.arviointi !== undefined) {
+          
+          Arviointi.get({arviointiId: tulos.arviointi['fi.vm.sade.eperusteet.domain.Arviointi']}, function(tulos) {
+            $scope.arviointi = tulos;
+
+          }, function(virhe) {
+            console.log(virhe.status);
+          });
+        }
+ 
+    }, function(virhe) {
+      console.log(virhe.status);
+      if (virhe.status === 404) {
+        //virhe.data
+      }
+    });
+  
+    $scope.valitseKieli = function(nimi) {
+      return YleinenData.valitseKieli(nimi);
+    };
+    
   });
--- eperusteet/eperusteet-app/yo/test/spec/controllers/tutkinnonosa.js
+++ eperusteet/eperusteet-app/yo/test/spec/controllers/tutkinnonosa.js
@@ -16,7 +16,7 @@ describe('Controller: TutkinnonosaCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should work', function () {
+  	//TODO
   });
 });
