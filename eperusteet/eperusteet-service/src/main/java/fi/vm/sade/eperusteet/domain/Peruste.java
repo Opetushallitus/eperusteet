c3b3bde7f709953112bbce9a7a3633505b433033
Harri Kemppi <harri.kemppi@gofore.com>
2014-02-19 09:24:25 +0200

    KoodiUri kenttä lisätty Perusteeseen

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/domain/Peruste.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/domain/Peruste.java
@@ -1,6 +1,17 @@
 /*
- * Copyright Gofore Oy.
- * http://www.gofore.com/
+ * Copyright (c) 2013 The Finnish Board of Education - Opetushallitus
+ * 
+ * This program is free software: Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ * 
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * European Union Public Licence for more details.
  */
 package fi.vm.sade.eperusteet.domain;
 
@@ -33,6 +44,9 @@ public class Peruste implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    
+    private String koodiUri;
+    
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private TekstiPalanen nimi;
     private String tutkintokoodi;
@@ -62,6 +76,14 @@ public class Peruste implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getKoodiUri() {
+        return koodiUri;
+    }
+
+    public void setKoodiUri(String koodiUri) {
+        this.koodiUri = koodiUri;
     }
 
     public TekstiPalanen getNimi() {
