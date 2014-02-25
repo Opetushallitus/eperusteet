5bb1ec93dd9a6fe3a5b9053485e68bcad9e919b7
Harri Kemppi <harri.kemppi@gofore.com>
2014-02-25 07:57:25 +0200

    Lisätty mockattu koulutusalaSErvice testeihin.

--- eperusteet/eperusteet-app/yo/test/spec/controllers/haku.js
+++ eperusteet/eperusteet-app/yo/test/spec/controllers/haku.js
@@ -6,10 +6,25 @@ describe('Controller: HakuCtrl', function () {
   beforeEach(module('eperusteApp'));
 
   var SearchCtrl,
-    scope;
+    scope, 
+    koulutusalaService;
+    
+  // Mock koulutusalaService
+  beforeEach(module(function($provide) {
+    koulutusalaService = {
+      haeKoulutusalat: function() {
+        return [];
+      },
+      haeKoulutusalaNimi: function(koodi) {
+        return {};
+      }
+    };
+    $provide.value('koulutusalaService', koulutusalaService);
+  }));
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
+
     scope = $rootScope.$new();
     SearchCtrl = $controller('HakuCtrl', {
       $scope: scope
