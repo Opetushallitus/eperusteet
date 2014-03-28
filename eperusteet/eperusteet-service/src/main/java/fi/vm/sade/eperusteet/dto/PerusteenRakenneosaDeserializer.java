b964082ffeb061667abc3c2ed14d923436cef5fd
Teemu Leinonen <teemu.leinonen@cybercom.com>
2014-03-28 14:52:30 +0200

    Kehitetty deserializer-luokkaa ja poistettu sieltä kovakoodatut jäsenmuuttujien nimet.

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/PerusteenRakenneosaDeserializer.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/PerusteenRakenneosaDeserializer.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,64 +15,64 @@ import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
 
 public class PerusteenRakenneosaDeserializer extends JsonDeserializer<AbstractRakenneosaDto> {
 
 	private static final Logger LOG = LoggerFactory.getLogger(PerusteenRakenneosaDeserializer.class);
-	
-	@SuppressWarnings("serial")
-	private static final Map<String, Class<?>> propertyClassMap = new HashMap<String, Class<?>>() {{
-		put("otsikko", LokalisoituTekstiDto.class);
-		put("saannot", SaannostoDto.class);
-		put("osat", List.class);
-		put("_perusteenOsa", EntityReference.class);
-	}};
-	
+
 	@Override
 	public AbstractRakenneosaDto deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-		LOG.debug("{}", ctxt.getConfig().introspect(ctxt.constructType(RakenteenLehtiDto.class)).findProperties());
-		
+		Map<String, BeanPropertyDefinition> definitionMap = getPropertyDefinitionsFor(RakenteenLehtiDto.class, ctxt);
 		ObjectMapper mapper = (ObjectMapper) jp.getCodec();
-		LOG.debug("alku");
+		LOG.debug("start parsing");
+
 		if (jp.getCurrentToken() == JsonToken.START_OBJECT) {
-			Boolean isLeaf = null;
-			
+			Boolean isLeaf = Boolean.TRUE;
+
 			Map<String, Object> props = new HashMap<>();
 			while (jp.nextToken() != JsonToken.END_OBJECT) {
-				LOG.debug("{}", jp.getCurrentToken());
+				LOG.debug("token: {}", jp.getCurrentToken());
 				String propKey = jp.getCurrentName();
-				LOG.debug(propKey);
-				if(jp.getCurrentToken().isStructStart()) {
-					if("osat".equals(propKey)) {
+				LOG.debug("prop name: {}", propKey);
+				if (jp.getCurrentToken().isStructStart()) {
+					if (!definitionMap.containsKey(propKey)) {
+						definitionMap = getPropertyDefinitionsFor(RakenteenHaaraDto.class, ctxt);
 						isLeaf = Boolean.FALSE;
-					} else if("_perusteenOsa".equals(propKey)) {
-						isLeaf = Boolean.TRUE;
 					}
-					
-					Object prop = mapper.readValue(jp, propertyClassMap.get(propKey));
-					LOG.debug("{}", prop);
+					LOG.debug("field class: {}", definitionMap.get(propKey).getField().getRawType());
+					Object prop = mapper.readValue(jp, definitionMap.get(propKey).getField().getRawType());
+					LOG.debug("found prop: {}", prop);
 					props.put(propKey, prop);
-					
-				} 
+
+				}
 			}
-			
-			if(Boolean.TRUE.equals(isLeaf)) {
-				RakenteenLehtiDto dto = new RakenteenLehtiDto();
-				dto.setOtsikko((LokalisoituTekstiDto) props.get("otsikko"));
-				dto.setSaannot((SaannostoDto) props.get("saannot"));
-				dto.setPerusteenOsa((EntityReference) props.get("_perusteenOsa"));
-				return dto;
-			} else if(Boolean.FALSE.equals(isLeaf)) {
-				RakenteenHaaraDto dto = new RakenteenHaaraDto();
-				dto.setOtsikko((LokalisoituTekstiDto) props.get("otsikko"));
-				dto.setSaannot((SaannostoDto) props.get("saannot"));
-				dto.setOsat((List<AbstractRakenneosaDto>) props.get("osat"));
-				return dto;
+			AbstractRakenneosaDto completeDto = null;
+			if (Boolean.TRUE.equals(isLeaf)) {
+				completeDto = new RakenteenLehtiDto();
+
+			} else if (Boolean.FALSE.equals(isLeaf)) {
+				completeDto = new RakenteenHaaraDto();
 			}
+
+			for (Entry<String, Object> entry : props.entrySet()) {
+				LOG.debug("field: {}, field value: {}, annotated method: {}", entry.getKey(), entry.getValue(), definitionMap.get(entry.getKey()).getSetter());
+				definitionMap.get(entry.getKey()).getSetter().setValue(completeDto, entry.getValue());
+			}
+
+			return completeDto;
 		}
 		return null;
-
 	}
 
-	
+	private Map<String, BeanPropertyDefinition> getPropertyDefinitionsFor(Class<?> beanClass, DeserializationContext ctxt) {
+		Map<String, BeanPropertyDefinition> definitionMap = new HashMap<>();
+		List<BeanPropertyDefinition> propertyDefinitions = ctxt.getConfig().introspect(ctxt.constructType(beanClass)).findProperties();
+		for (BeanPropertyDefinition def : propertyDefinitions) {
+			LOG.debug("name: {}, full name: {}, setter: {}", def.getName(), def.getFullName(), def.getSetter());
+			definitionMap.put(def.getName(), def);
+		}
+
+		return definitionMap;
+	}
 }
