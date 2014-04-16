3130afd1892197f82970b038b39531b7ebe87d4e
Niko Junkala <niko.junkala@gofore.com>
2014-04-16 17:13:43 +0300

    Lisätty ohjeistusta navigaatiopolun käyttöön

--- eperusteet/eperusteet-app/yo/app/scripts/services/navigaatiopolku.js
+++ eperusteet/eperusteet-app/yo/app/scripts/services/navigaatiopolku.js
@@ -1,6 +1,21 @@
 'use strict';
 /* global _ */
 
+// Tila-asetukset:
+// $stateProvider.state('', { ... asetus: optio, ... });
+//
+// naviBase []:
+// Tyhjentää navin ja asettaa naviBasen listan naviksi
+//
+// naviRest []:
+// Asettaa naviksi edellisen/voimassaolevan naviBasen ja yhdistää naviRestin sen perään
+//
+// Kummallekin voi antaa merkkijonona muuttujan (alkaa ':':lla), jonka tilalle haetaan
+// kyseinen muuttuja $stateParamsista tilan vaihtuessa
+//
+// Navigaatiopolku.asetaElementit-funktiolla voi asettaa näiden merkkijonomuuttujien
+// tilalle muita merkkijonoja, esimerkiksi voisit haluta korvata id:n nimellä.
+
 angular.module('eperusteApp')
   .service('Navigaatiopolku', function($rootScope, $state) {
     var naviElementit = {};
@@ -9,12 +24,11 @@ angular.module('eperusteApp')
     $rootScope.naviRest = [];
 
     function navigaatioMap(toState, toParams) {
-      var defcon = _.merge({
-        append: false
-      }, toState.naviConfig);
+      var defcon = _.merge({ append: false }, toState.naviConfig);
 
       function relink(link) {
         var nlink = link;
+
         if (_.first(link) === ':') {
           var kentta = link.substr(1);
           nlink = naviElementit[kentta] ? naviElementit[kentta] : toParams[kentta];
@@ -26,45 +40,22 @@ angular.module('eperusteApp')
         };
       }
 
-      if (toState.naviBase) {
-        $rootScope.naviBase = _.map(toState.naviBase, relink);
-      }
-
       var rest = [];
-      if (toState.naviRest) {
-        rest = _.map(toState.naviRest, relink);
-      }
-
+      if (toState.naviBase) { $rootScope.naviBase = _.map(toState.naviBase, relink); }
+      if (toState.naviRest) { rest = _.map(toState.naviRest, relink); }
       $rootScope.naviRest = defcon.append ? $rootScope.naviRest.concat(rest) : rest;
     }
 
     function paivitaNavigaatio() {
       function paivita(l) {
         var nl = naviElementit[l.kentta.substr(1)];
-        if (nl) {
-          l.arvo = nl;
-        }
+        if (nl) { l.arvo = nl; }
       }
       _.forEach($rootScope.naviBase, paivita);
       _.forEach($rootScope.naviRest, paivita);
       naviPolku = _.filter($rootScope.naviBase.concat($rootScope.naviRest), function(link) { return !_.isEmpty(link); });
       $rootScope.$emit('naviUpdate');
     }
-
-    this.haeNavipolku = function() {
-      return _.clone(naviPolku);
-    };
-
-    this.asetaElementit = function(elements) {
-      naviElementit = _.merge(_.clone(naviElementit), elements);
-      paivitaNavigaatio();
-    };
-
-    this.clear = function() {
-      naviPolku = [];
-      $rootScope.naviBase = [];
-      $rootScope.naviRest = [];
-    };
 
     $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams) {
       if (_.isEmpty($rootScope.naviBase) && _.isEmpty($rootScope.naviRest)) {
@@ -78,5 +69,20 @@ angular.module('eperusteApp')
       navigaatioMap(toState, toParams);
       paivitaNavigaatio();
     });
-    return this;
+
+    return {
+      // Palauttaa populoidun navipolun - kotisivu
+      haeNavipolku: function() { return _.clone(naviPolku); },
+      // Asettaa :muuttujan näkyvän arvon
+      asetaElementit: function(elements) {
+        naviElementit = _.merge(_.clone(naviElementit), elements);
+        paivitaNavigaatio();
+      },
+      // Tyhjentää navipolun täysin
+      clear: function() {
+        naviPolku = [];
+        $rootScope.naviBase = [];
+        $rootScope.naviRest = [];
+      }
+    };
   });
