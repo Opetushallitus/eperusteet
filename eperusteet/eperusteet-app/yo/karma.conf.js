772017dc34265bc36bd31e800d08e7e70ac6fce8
Harri Kemppi <harri.kemppi@gofore.com>
2014-04-29 09:31:48 +0300

    Ui-sortablen polku muutettu myös karma.conf.js:ään

--- eperusteet/eperusteet-app/yo/karma.conf.js
+++ eperusteet/eperusteet-app/yo/karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function(config) {
       'app/bower_components/angular-ui-bootstrap-bower/ui-bootstrap.js',
       'app/bower_components/angular-ui-bootstrap-bower/ui-bootstrap-tpls.js',
       'app/bower_components/angular-ui-utils/ui-utils.js',
-      'app/bower_components/angular-ui-sortable/sortable.js',
+      'app/bower_components/angular-ui-sortable/src/sortable.js',
       'app/bower_components/angular-ui-router/release/angular-ui-router.js',
       'app/bower_components/ckeditor/ckeditor.js',
       'app/bower_components/lodash/dist/lodash.js',
