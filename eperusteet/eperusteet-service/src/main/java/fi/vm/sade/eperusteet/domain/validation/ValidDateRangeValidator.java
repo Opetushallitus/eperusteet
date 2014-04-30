c1d51bbdc4e83608160f2cdd738956ae20a80f29
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2014-04-30 12:56:39 +0300

    ValidDateRangeValidator refaktorointi

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/domain/validation/ValidDateRangeValidator.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/domain/validation/ValidDateRangeValidator.java
@@ -13,11 +13,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * European Union Public Licence for more details.
  */
-
 package fi.vm.sade.eperusteet.domain.validation;
 
-import fi.vm.sade.eperusteet.domain.Perusteprojekti;
-import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -28,53 +26,41 @@ import org.slf4j.LoggerFactory;
  *
  * @author harrik
  */
-public class ValidDateRangeValidator implements ConstraintValidator<ValidateDateRange, Perusteprojekti> {
-    private String start;
-    private String end;
+public class ValidDateRangeValidator implements ConstraintValidator<ValidateDateRange, Object> {
+
+    private static final Class<?>[] EMPTY_TYPE_ARRAY = new Class<?>[0];
+    private String startMethodName;
+    private String endMethodName;
 
     private static final Logger LOG = LoggerFactory.getLogger(ValidDateRangeValidator.class);
-    
+
     @Override
     public void initialize(ValidateDateRange validateDateRange) {
-        start = validateDateRange.start();
-        end = validateDateRange.end();
+        startMethodName = getAccessorMethodName(validateDateRange.start());
+        endMethodName = getAccessorMethodName(validateDateRange.end());
     }
 
     @Override
-    public boolean isValid(Perusteprojekti perusteprojekti, ConstraintValidatorContext cvc) {
-        try {
-            Class clazz = perusteprojekti.getClass();
-            Date startDate = null;
-            Method startGetter = clazz.getMethod(getAccessorMethodName(start), new Class[0]);
-            Object startGetterResult = startGetter.invoke(perusteprojekti, null);
-            
-            if (startGetterResult == null) {
-                return true;
-            }
-            if (startGetterResult instanceof Date){
-                startDate = (Date) startGetterResult;
-            }
-            
-            Date endDate = null;
-            Method endGetter = clazz.getMethod(getAccessorMethodName(end), new Class[0]);
-            Object endGetterResult = endGetter.invoke(perusteprojekti, null);
-            if (endGetterResult == null){
-                return true;
-            }
-            if (endGetterResult instanceof Date){
-                endDate = (Date) endGetterResult;
-            }
-            return (startDate.before(endDate));           
-        } catch (Throwable e) {
-            LOG.error("ValidDateRangeValidator virhe", e);
+    public boolean isValid(Object obj, ConstraintValidatorContext cvc) {
+        Date startDate = invokeDateGetter(obj, startMethodName);
+        Date endDate = invokeDateGetter(obj, endMethodName);
+        if (startDate == null || endDate == null) {
+            return true;
         }
-
-        return false;
+        return (startDate.before(endDate));
     }
-    
-    private String getAccessorMethodName(String property){
+
+    private static Date invokeDateGetter(Object obj, String methodName) {
+        try {
+            return (Date) obj.getClass().getMethod(methodName, EMPTY_TYPE_ARRAY).invoke(obj, (Object) null);
+        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | ClassCastException e) {
+            throw new IllegalArgumentException("Unable to access " + obj.getClass().getName() + "#" + methodName, e);
+        }
+    }
+
+    private static String getAccessorMethodName(String property) {
         StringBuilder builder = new StringBuilder("get");
-        builder.append(Character.toUpperCase(property.charAt(0))); 
+        builder.append(Character.toUpperCase(property.charAt(0)));
         builder.append(property.substring(1));
         return builder.toString();
     }
