8f69e4bce95888ac48d1295836cdc842c7b71f93
Niko Junkala <niko.junkala@gofore.com>
2014-06-04 20:03:58 +0300

    Rakenteen tutkinnon osa lista ei enää muutu dragatessa

--- eperusteet/eperusteet-app/yo/app/scripts/directives/tree.js
+++ eperusteet/eperusteet-app/yo/app/scripts/directives/tree.js
@@ -16,6 +16,7 @@
 
 'use strict';
 /*global _*/
+/*global $*/
 
 angular.module('eperusteApp')
   .service('TreeCache', function() {
@@ -269,6 +270,12 @@ angular.module('eperusteApp')
           disabled: !scope.muokkaus,
           placeholder: 'placeholder',
           tolerance: 'pointer',
+          // http://stackoverflow.com/questions/6940390/how-do-i-duplicate-item-when-using-jquery-sortable
+          helper: function(el, ui) {
+            this.copyHelper = ui.clone().insertAfter(ui);
+            $(this).data('copied', false);
+            return ui.clone();
+          },
           stop: function() {
             paivitaUniikit();
           },
