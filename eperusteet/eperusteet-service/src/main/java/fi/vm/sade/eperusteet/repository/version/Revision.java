d06744afd215c2260d94b1dbbb65e736be0a315e
Niko Junkala <niko.junkala@gofore.com>
2014-08-05 13:16:51 +0300

    Korjattu sisennys

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/repository/version/Revision.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/repository/version/Revision.java
@@ -25,47 +25,54 @@ import lombok.Setter;
 @Setter
 public class Revision implements Serializable {
 
-	private Integer number;
-	private Date date;
+    private Integer number;
+    private Date date;
     private String muokkaajaOid;
     private String kommentti;
 
-	public Revision(Integer number, Long timestamp, String muokkaajaOid, String kommentti) {
-		this.number = number;
-		this.date = new Date(timestamp);
+    public Revision(Integer number, Long timestamp, String muokkaajaOid, String kommentti) {
+        this.number = number;
+        this.date = new Date(timestamp);
         this.muokkaajaOid = muokkaajaOid;
         this.kommentti = kommentti;
-	}
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((date == null) ? 0 : date.hashCode());
-		result = prime * result + ((number == null) ? 0 : number.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((date == null) ? 0 : date.hashCode());
+        result = prime * result + ((number == null) ? 0 : number.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Revision other = (Revision) obj;
-		if (date == null) {
-			if (other.date != null)
-				return false;
-		} else if (!date.equals(other.date))
-			return false;
-		if (number == null) {
-			if (other.number != null)
-				return false;
-		} else if (!number.equals(other.number))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Revision other = (Revision) obj;
+        if (date == null) {
+            if (other.date != null) {
+                return false;
+            }
+        } else if (!date.equals(other.date)) {
+            return false;
+        }
+        if (number == null) {
+            if (other.number != null) {
+                return false;
+            }
+        } else if (!number.equals(other.number)) {
+            return false;
+        }
+        return true;
+    }
 
 }
