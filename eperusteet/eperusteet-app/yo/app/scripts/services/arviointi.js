4961e7a5c63e0e1ce752db6ec4c30c7ac6a8e158
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2014-08-22 09:24:25 +0300

    poistettu turha arviointi-API

--- eperusteet/eperusteet-app/yo/app/scripts/services/arviointi.js
+++ eperusteet/eperusteet-app/yo/app/scripts/services/arviointi.js
@@ -17,11 +17,7 @@
 'use strict';
 
 angular.module('eperusteApp')
-  .factory('Arviointi', function($resource, SERVICE_LOC) {
-    return $resource(SERVICE_LOC + '/arvioinnit/:arviointiId', {
-      arviointiId: '@id'
-    });
-  }).factory('Arviointiasteikot', function($resource, SERVICE_LOC) {
+  .factory('Arviointiasteikot', function($resource, SERVICE_LOC) {
     return $resource(SERVICE_LOC + '/arviointiasteikot/:asteikkoId', {
       asteikkoId: '@id'
     });
