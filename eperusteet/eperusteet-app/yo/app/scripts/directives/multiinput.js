7bf2959da258ff47b95d9634c5ef6415e8921d98
Niko Junkala <niko.junkala@gofore.com>
2014-10-06 16:26:21 +0300

    Korjattu scopeen liittyvä bugi

--- eperusteet/eperusteet-app/yo/app/scripts/directives/multiinput.js
+++ eperusteet/eperusteet-app/yo/app/scripts/directives/multiinput.js
@@ -29,37 +29,36 @@ angular.module('eperusteApp')
       },
       templateUrl: 'views/multiinput.html',
       replace: true,
-      link: function (scope, element) {
-        scope.type = scope.type || 'text';
-        scope.isObject = _.isObject(scope.mlData);
-        scope.langs = _(_.values(YleinenData.kielet)).union(scope.mlAdditionalLanguages || [])
-                                                      .sort()
-                                                      .value();
+      transclude: true,
+      controller: function($scope) {
+        $scope.type = $scope.type || 'text';
+        $scope.langs = _(_.values(YleinenData.kielet)).union($scope.mlAdditionalLanguages || [])
+                                                     .sort()
+                                                     .value();
+        _.forEach($scope.langs, function(lang) {
+          $scope.mlData[lang] = $scope.mlData[lang] || '';
+        });
 
+        $scope.activeLang = $translate.use() || $translate.preferredLanguage();
+        $scope.kielivalintaAuki = false;
+
+        $scope.vaihdaKieli = function(uusiKieli) {
+          $scope.kielivalintaAuki = false;
+          $scope.activeLang = uusiKieli;
+        };
+      },
+      link: function(scope, element) {
         if (!scope.mlData) {
           console.log('You must set ml-data for ml-input.');
         }
-
-        if (!scope.isObject) {
+        else if (!_.isObject(scope.mlData)) {
           console.log('ml-data must be an object');
         }
-
-        _.forEach(scope.langs, function(lang) {
-          scope.mlData[lang] = scope.mlData[lang] || '';
-        });
-        scope.activeLang = $translate.use() || $translate.preferredLanguage();
-        scope.kielivalintaAuki = false;
-
-        scope.vaihdaKieli = function(uusiKieli) {
-          scope.kielivalintaAuki = false;
-          scope.activeLang = uusiKieli;
-        };
         if (scope.$parent.inputElId) {
           $timeout(function () {
             element.find('input, textarea').attr('id', scope.$parent.inputElId);
           });
         }
-
       }
     };
   });
