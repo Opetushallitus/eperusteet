52a2481aa038970f0a17e78cf5dfac355497577b
Niko Junkala <niko.junkala@gofore.com>
2014-11-03 13:45:50 +0200

    Lisätty TutkintonimikeKoodiDto

---
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/peruste/TutkintonimikeKoodiDto.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2013 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software: Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * European Union Public Licence for more details.
+ */
+package fi.vm.sade.eperusteet.dto.peruste;
+
+import fi.vm.sade.eperusteet.dto.util.EntityReference;
+
+/**
+ *
+ * @author nkala
+ */
+public class TutkintonimikeKoodiDto {
+    private Long id;
+    private EntityReference peruste;
+    private String tutkinnonOsaUri;
+    private String tutkinnonOsaArvo;
+    private String osaamisalaUri;
+    private String osaamisalaArvo;
+    private String tutkintonimikeUri;
+    private String tutkintonimikeArvo;
+}
