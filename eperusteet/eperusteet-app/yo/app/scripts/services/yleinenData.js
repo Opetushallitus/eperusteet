9879dabb180e4e511eece8af92108789a0fb6db2
Antti Pekka Vilkko <antti.pekka.vilkko@gofore.com>
2014-11-25 13:06:21 +0200

    Tilakuvauksille ja -siirtymille lokalisointiavaimet

--- eperusteet/eperusteet-app/yo/app/scripts/services/yleinenData.js
+++ eperusteet/eperusteet-app/yo/app/scripts/services/yleinenData.js
@@ -28,9 +28,6 @@ angular.module('eperusteApp')
 
     this.naviOmit = ['root', 'editoi', 'suoritustapa', 'sisalto', 'aloitussivu', 'selaus', 'esitys'];
 
-    // TODO: poista joskus
-    this.loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-
     this.kontekstit = ['ammatillinenperuskoulutus',
       'ammatillinenaikuiskoulutus'];
 
@@ -51,21 +48,15 @@ angular.module('eperusteApp')
       ov: 'OPINTOVIIKKO',
     };
 
-    $rootScope.
-      this.kontekstit = [
-        'ammatillinenperuskoulutus',
-        'ammatillinenaikuiskoulutus'
-      ];
-
-      this.tilakuvaukset = [
-        'poistettu',
-        //'pohja',
-        'laadinta',
-        'kommentointi',
-        'viimeistely',
-        'kaannos',
-        'hyvaksytty'
-      ];
+    this.tilakuvaukset = [
+      'poistettu',
+      //'pohja',
+      'laadinta',
+      'kommentointi',
+      'viimeistely',
+      'kaannos',
+      'hyvaksytty'
+    ];
 
     this.suoritustavat = [
       'ops',
