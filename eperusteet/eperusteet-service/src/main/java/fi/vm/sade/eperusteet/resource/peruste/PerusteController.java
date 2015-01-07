c4e901fbd3d9f1f36c45cd93ea7e47c637bc06aa
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2015-01-07 13:46:12 +0200

    swagger version muutos

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/resource/peruste/PerusteController.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/resource/peruste/PerusteController.java
@@ -36,7 +36,6 @@ import fi.vm.sade.eperusteet.service.PerusteService;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import javax.ws.rs.GET;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
