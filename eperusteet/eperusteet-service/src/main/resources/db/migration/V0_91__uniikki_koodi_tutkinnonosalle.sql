07e0a84d9b091c5bf5979bc6a07e5f6a7c840648
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2015-01-15 11:41:20 +0200

    poista tutkinnonosan koodin uniikkirajoite.

--- eperusteet/eperusteet-service/src/main/resources/db/migration/V0_91__uniikki_koodi_tutkinnonosalle.sql
+++ eperusteet/eperusteet-service/src/main/resources/db/migration/V0_91__uniikki_koodi_tutkinnonosalle.sql
@@ -1,4 +1,4 @@
 UPDATE tutkinnonosa SET koodi_uri = NULL WHERE koodi_uri = '';
 
-ALTER TABLE tutkinnonosa
-    ADD CONSTRAINT uk_uniikki_tutkinnonosan_koodi UNIQUE (koodi_uri);
+#ALTER TABLE tutkinnonosa
+#    ADD CONSTRAINT uk_uniikki_tutkinnonosan_koodi UNIQUE (koodi_uri);
