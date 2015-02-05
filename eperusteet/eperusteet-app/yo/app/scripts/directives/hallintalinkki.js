126caad5e80282309aa26941bbaf13e482167e38
Harri Kemppi <harri.kemppi@gofore.com>
2015-02-05 14:54:36 +0200

    Laitettu hallintalinkki näkymään kaikille. Lisätty logitulostuksia.

--- eperusteet/eperusteet-app/yo/app/scripts/directives/hallintalinkki.js
+++ eperusteet/eperusteet-app/yo/app/scripts/directives/hallintalinkki.js
@@ -9,14 +9,20 @@ angular.module('eperusteApp')
       link: function postLink(scope, element) {
 
         if ($window.location.host.indexOf('localhost') === 0) {
+          console.log('show hallintalinkki');
           element.show();
         } else {
-          element.hide();
+          console.log('hide hallintalinkki');
+          element.show();
         }
+
         scope.$on('fetched:casTiedot', function() {
+          console.log('cas tiedot haettu', Profiili.groups());
           if (_.contains(Profiili.groups(), 'APP_EPERUSTEET_CRUD_1.2.246.562.10.00000000001')) {
+            console.log('ryhmä löyty');
             element.show();
           }
+
         });
 
       }
