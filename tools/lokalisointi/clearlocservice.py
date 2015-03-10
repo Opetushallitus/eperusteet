3e61c32756a5c597770b4a53f0b1a0785ec05f37
Niko Junkala <niko.junkala@gofore.com>
2015-03-10 16:09:08 +0200

    Lisätty skriptit lokalisaatioiden hallintaan

---
+++ tools/lokalisointi/clearlocservice.py
@@ -0,0 +1,24 @@
+#!/bin/python
+
+import subprocess
+import json
+import sys
+import http.client
+
+LOCALIZATION_SERVICE='itest-virkailija.oph.ware.fi'
+conn = http.client.HTTPSConnection(LOCALIZATION_SERVICE)
+conn.request('GET', '/lokalisointi/cxf/rest/v1/localisation?category=eperusteet')
+response = json.JSONDecoder().decode(conn.getresponse().read().decode())
+
+if len(sys.argv) < 2:
+    print('Usage: ' + sys.argv[0] + ' <JSESSIONID>')
+    sys.exit(2)
+
+for loc in response:
+    id = str(loc['id'])
+    if len(id) > 0 and loc.get('category') == 'eperusteet':
+        subprocess.call(['curl',
+                        '-X', 'DELETE',
+                        '-H', 'Content-Type: application/json',
+                        '-b', 'JSESSIONID=CBCE1CF54AC6C4484B653AF95116E542',
+                        'https://' + LOCALIZATION_SERVICE + '/lokalisointi/cxf/rest/v1/localisation/' + id])
---
+++ tools/lokalisointi/excel2locale.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+### excel2locale.py
+### Uploads a locale excel to localization service
+
+import subprocess
+import json
+import openpyxl
+import sys
+
+LOCALIZATION_SERVICE='https://itest-virkailija.oph.ware.fi/lokalisointi/cxf/rest/v1/localisation'
+
+if len(sys.argv) < 3:
+    print('Usage: ' + sys.argv[0] + '<JSESSIONID> <from.xlsx>')
+    sys.exit(2)
+
+def send_to_localization_service(key, langs):
+    for lang in langs:
+        if langs[lang]:
+            entry = {
+                'category': 'eperusteet',
+                'key': key,
+                'locale': lang,
+                'value': langs[lang]
+            }
+            subprocess.call(['curl',
+                            '-X', 'POST',
+                            '-H', 'Content-Type: application/json',
+                            '-b', 'JSESSIONID=' + sys.argv[1],
+                            '-d', json.JSONEncoder().encode(entry),
+                            LOCALIZATION_SERVICE])
+
+# Open worksheet
+workbook = openpyxl.load_workbook(sys.argv[2])
+worksheet = workbook.active
+
+# Send into localization service
+idx = 1
+for row in worksheet.rows:
+    if idx > 1:
+        if not row[0].value:
+            break
+        else:
+            send_to_localization_service(row[0].value, {
+                "fi": row[1].value, 
+                "sv": row[2].value, 
+                "en": row[3].value
+            })
+    idx += 1
