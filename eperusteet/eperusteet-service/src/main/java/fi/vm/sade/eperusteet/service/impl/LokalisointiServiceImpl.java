2ab3f82822fb206e2d9cb64c735b87aa2bbaa837
Jussi Niskanen <jussi.niskanen@gofore.com>
2015-03-13 14:04:09 +0200

    Poistettu @Profile -annotaation LokalisointiServiceImpl:stä

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/impl/LokalisointiServiceImpl.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/impl/LokalisointiServiceImpl.java
@@ -21,7 +21,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -30,7 +29,6 @@ import org.springframework.web.client.RestTemplate;
  * @author jussi
  */
 @Service
-@Profile(value = "default")
 public class LokalisointiServiceImpl implements LokalisointiService {
 
     private static final Logger LOG = LoggerFactory.getLogger(LokalisointiService.class);
