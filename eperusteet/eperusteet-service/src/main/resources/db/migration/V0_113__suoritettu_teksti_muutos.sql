22ab9df5c7ca4b3685154273cf8d02d18e6b832b
Harri Kemppi <harri.kemppi@gofore.com>
2015-06-23 14:38:21 +0300

    Korjaus issueen EP-464, pois lukien PDF.

---
+++ eperusteet/eperusteet-service/src/main/resources/db/migration/V0_113__suoritettu_teksti_muutos.sql
@@ -0,0 +1,2 @@
+update tekstipalanen_teksti set teksti = 'Hyväksytyn suorituksen kriteerit' WHERE kieli = 'FI' AND teksti = 'Suoritettu S';
+update tekstipalanen_teksti set teksti = '[Hyväksytyn suorituksen kriteerit]' WHERE kieli = 'SV' AND teksti = '[Suoritettu S]';
