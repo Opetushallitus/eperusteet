7554bc60fde4fa34692c95b3ae5da30ce2c38bd7
Niko Junkala <niko.junkala@gofore.com>
2016-09-06 13:21:36 +0300

    Yritys korjata virheellinen kysely

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/repository/PerusteRepository.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/repository/PerusteRepository.java
@@ -41,7 +41,7 @@ public interface PerusteRepository extends JpaWithVersioningRepository<Peruste, 
         "LEFT JOIN p.perusopetuksenPerusteenSisalto ps " +
         "LEFT JOIN p.lukiokoulutuksenPerusteenSisalto ls " +
         "LEFT JOIN p.esiopetuksenPerusteenSisalto eps " +
-        "WHERE p.tila = ?2 AND (s.sisalto.id IN ?1 OR ps.sisalto.id IN ?1 OR eps.sisalto.id IN ?1 OR ls.id IN ?1)")
+        "WHERE p.tila = ?2 AND (s.sisalto.id IN (?1) OR ps.sisalto.id IN (?1) OR eps.sisalto.id IN (?1) OR ls.id IN (?1) )")
     Set<Long> findBySisaltoRoots(Iterable<? extends Number> rootIds, PerusteTila tila);
 
     @Query("SELECT DISTINCT p.id FROM Peruste p JOIN p.suoritustavat s JOIN s.tutkinnonOsat to WHERE p.tila = ?2 AND to.tutkinnonOsa.id = ?1")
