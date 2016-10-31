49ae37f873b754c260f5e07315b4d34f551a2922
Niko Junkala <niko.junkala@gofore.com>
2016-10-31 17:48:21 +0200

    Lisää testejä

--- eperusteet/eperusteet-app/yo/e2e/e2e/perusteprojektit.ts
+++ eperusteet/eperusteet-app/yo/e2e/e2e/perusteprojektit.ts
@@ -1,9 +1,49 @@
-const assertFailedHandles = (str: string) => expect(str.startsWith("{{") && str.endsWith("}}")).toBeFalsy();
-
-describe("Sivuston näkymä", () => {
-    it("etusivu latautuu", () => {
-        browser.get("http://localhost:9000/");
-        browser.driver.manage().window().setSize(1024, 768);
-        browser.waitForAngular();
+describe("Perusteprojekti", () => {
+    it("Kirjautuminen hallinnalla", () => {
+        return browser.get("http://test:test@localhost:9000/")
+            .then(() => browser.get("http://localhost:9000/"));
     });
+
+    it("Luonti - Ammatillinen", () => {
+        return PerusteHelpers.luoPerusteprojekti("ammatillinen", 2);
+    });
+
+    it("Luonti - Julkaisukuntoon", () => {
+        return PerusteHelpers.asetaJulkaisuvalmius();
+    });
+
+    it("Luonti - Tekstikappaleen lisääminen", () => {
+        return PerusteHelpers.lisaaTekstikappale("foobar", "Hello world");
+    });
+
+    it("Luonti - Tutkinnon osan lisääminen", () => {
+        return PerusteHelpers.lisaaTutkinnonOsa("Testi tutkinnon osa");
+    });
+
+});
+
+describe("Perusteprojektin julkaisu", () => {
+    it("Kirjautuminen hallinnalla", () => {
+        browser.get("http://localhost:9000/");
+        return browser.waitForAngular();
+    });
+
+    it("Luonti", () => PerusteHelpers.luoPerusteprojekti("ammatillinen", 2));
+
+    it("Julkaisu", () => PerusteHelpers.asetaJulkaisuvalmius()
+        .then(() => PerusteHelpers.vaihdaTila("viimeistely"))
+        .then(() => PerusteHelpers.vaihdaTila("valmis"))
+        .then(() => PerusteHelpers.vaihdaTila("julkaistu")));
+
+    it("Julkaistun perustietojen muokkaus", () => PerusteHelpers.muokkaaPerusteenTiedot("Uusi nimi", "uusidiaari", "10.2.2238"));
+
+    it("Julkaisusta luonnokseen", () => PerusteHelpers.vaihdaTila("luonnos"));
+
+    it("Julkaistu -> luonnos perustietojen muokkaus", () => PerusteHelpers.muokkaaPerusteenTiedot("Vanha nimi", "vanhadiaari", "1.2.2238"));
+
+    it("Julkaisu uudestaan", () => PerusteHelpers.asetaJulkaisuvalmius()
+        .then(() => PerusteHelpers.vaihdaTila("viimeistely"))
+        .then(() => PerusteHelpers.vaihdaTila("valmis"))
+        .then(() => PerusteHelpers.vaihdaTila("julkaistu")));
+
 });
--- eperusteet/eperusteet-app/yo/e2e/initProtractor.sh
+++ eperusteet/eperusteet-app/yo/e2e/initProtractor.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 protractor="./node_modules/protractor"
+[[ ! -d $protractor ]] && npm install protractor
 tsc conf.ts
 tsc --outFile e2e/result.js
+$protractor/bin/webdriver-manager update --standalone
 $protractor/bin/webdriver-manager start 2> /dev/null &
 sleep 2
 $protractor/bin/protractor conf.js
