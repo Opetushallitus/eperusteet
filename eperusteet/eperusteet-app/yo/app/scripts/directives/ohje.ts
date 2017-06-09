ed6707cf1e8dd13f824b55782417d383d57fa0f8
Sauli Anto <sauli.anto@gofore.com>
2017-06-09 10:57:18 +0300

    Korjattu ohje ja tag-cloud direktiivit

--- eperusteet/eperusteet-app/yo/app/scripts/directives/ohje.ts
+++ eperusteet/eperusteet-app/yo/app/scripts/directives/ohje.ts
@@ -111,16 +111,13 @@ angular.module('eperusteApp')
   });
 
 // Modify popover template for binding unsafe html
-angular.module('template/popover/popover.html', []).run(['$templateCache', function ($templateCache) {
-  $templateCache.put('template/popover/popover.html',
-      '<div class="popover {{placement}}" ng-class="{ in: isOpen(), fade: animation() }">\n' +
-      '  <div class="arrow"></div>\n' +
-      '\n' +
-      '  <div class="popover-inner">\n' +
-      '      <h3 class="popover-title" ng-bind-html="title | unsafe" ng-show="title"></h3>\n' +
-      '      <div class="popover-content" ng-bind-html="content | unsafe"></div>\n' +
-      '      <div class="popover-extra"></div>\n' +
-      '  </div>\n' +
-      '</div>\n' +
-      '');
-}]);
+angular
+.module('uib/template/popover/popover.html', [])
+.run($templateCache => {
+    $templateCache.put('uib/template/popover/popover.html',
+        '<div class="arrow"></div>\n' +
+        '<div class="popover-inner">\n' +
+        '  <h3 class="popover-title" ng-bind-html="uibTitle | unsafe" ng-if="uibTitle"></h3>\n' +
+        '  <div class="popover-content" ng-bind-html="content | unsafe"></div>\n' +
+        '</div>\n');
+});
