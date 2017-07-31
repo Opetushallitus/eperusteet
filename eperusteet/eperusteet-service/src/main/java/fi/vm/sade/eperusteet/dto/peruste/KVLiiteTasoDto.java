4e958d8e64064f6cae283ef890b0903510aa65ae
Niko Junkala <niko.junkala@gofore.com>
2017-07-31 16:23:39 +0300

    kvliitteen tasot järjestykseen

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/peruste/KVLiiteTasoDto.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/peruste/KVLiiteTasoDto.java
@@ -30,4 +30,17 @@ public class KVLiiteTasoDto {
     String codeUri;
     String codeValue;
     LokalisoituTekstiDto nimi;
+
+    public int getJarjestys() {
+        if (codeUri.startsWith("nqf")) {
+            return 0;
+        }
+        else if (codeUri.startsWith("eqf")) {
+            return 1;
+        }
+        else if (codeUri.startsWith("isced")) {
+            return 2;
+        }
+        return 99;
+    }
 }
