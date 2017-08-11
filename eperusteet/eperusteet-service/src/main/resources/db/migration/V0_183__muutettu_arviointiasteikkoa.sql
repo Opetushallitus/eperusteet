676b8faf21eb06cc5cfa17fcffee9007ded28d62
Sauli Anto <sauli.anto@gofore.com>
2017-08-11 11:16:03 +0300

    Muutettu arviointiasteikon tekstejä

---
+++ eperusteet/eperusteet-service/src/main/resources/db/migration/V0_183__muutettu_arviointiasteikkoa.sql
@@ -0,0 +1,2 @@
+update tekstipalanen_teksti set teksti = 'Hyväksytty / Hylätty' WHERE kieli = 'FI' AND teksti = 'Hyväksytyn suorituksen kriteerit';
+update tekstipalanen_teksti set teksti = '[Hyväksytty / Hylätty]' WHERE kieli = 'SV' AND teksti = '[Hyväksytyn suorituksen kriteerit]';
