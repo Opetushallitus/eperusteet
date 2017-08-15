94fa28042789e680f42ea0b99d4ac0575828f260
Sauli Anto <sauli.anto@gofore.com>
2017-08-15 12:18:18 +0300

    Poistettu väärän haaran tiedosto

--- eperusteet/eperusteet-service/src/main/resources/db/migration/V0_183__muutettu_arviointiasteikkoa.sql
+++
@@ -1,2 +0,0 @@
-update tekstipalanen_teksti set teksti = 'Hyväksytty / Hylätty' WHERE kieli = 'FI' AND teksti = 'Hyväksytyn suorituksen kriteerit';
-update tekstipalanen_teksti set teksti = '[Hyväksytty / Hylätty]' WHERE kieli = 'SV' AND teksti = '[Hyväksytyn suorituksen kriteerit]';
