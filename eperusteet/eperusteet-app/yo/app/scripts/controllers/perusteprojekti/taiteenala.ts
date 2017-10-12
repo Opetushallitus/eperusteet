d075d09c0b6b5628a737a9162b9d9e4f649ff228
Niko Junkala <niko.junkala@gofore.com>
2017-10-12 17:33:48 +0300

    Otettu oikea koodisto käyttöön

--- eperusteet/eperusteet-app/yo/app/scripts/controllers/perusteprojekti/taiteenala.ts
+++ eperusteet/eperusteet-app/yo/app/scripts/controllers/perusteprojekti/taiteenala.ts
@@ -50,7 +50,7 @@ export function taiteenalaCtrl(
             },
             {
                 tyyppi: () => {
-                    return "tutkinnonosat";
+                    return "oppiaineetyleissivistava2";
                 },
                 ylarelaatioTyyppi: () => {
                     return "";
