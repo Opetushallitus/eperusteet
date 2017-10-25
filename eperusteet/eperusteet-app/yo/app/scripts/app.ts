c8c89756a7ead8274f5447d5ba97f5d95ac1acb9
Sauli Anto <isaul32@users.noreply.github.com>
2017-10-25 20:35:29 +0300

    Muutettu latauspalkki AMOSAA:n mukaiseksi (#71)

--- eperusteet/eperusteet-app/yo/app/scripts/app.ts
+++ eperusteet/eperusteet-app/yo/app/scripts/app.ts
@@ -38,7 +38,8 @@ angular
         "ngFileUpload",
         "eGenericTree",
         "eMathDisplay",
-        "LocalStorageModule"
+        "LocalStorageModule",
+        "angular-loading-bar"
     ])
     .constant("SERVICE_LOC", "/eperusteet-service/api")
     .constant("ORGANISATION_SERVICE_LOC", "/lokalisointi/cxf/rest/v1/localisation")
@@ -53,7 +54,8 @@ angular
     .constant("TEXT_HIERARCHY_MAX_DEPTH", 8)
     .constant("SHOW_VERSION_FOOTER", true)
     .constant("DEVELOPMENT", process.env.NODE_ENV === "development")
-    .config(($sceProvider, $urlRouterProvider, $translateProvider, $urlMatcherFactoryProvider, $locationProvider) => {
+    .config(($sceProvider, $urlRouterProvider, $translateProvider, $urlMatcherFactoryProvider, $locationProvider,
+             cfpLoadingBarProvider) => {
         const preferred = "fi";
 
         $sceProvider.enabled(true);
@@ -73,6 +75,10 @@ angular
         $translateProvider.useSanitizeValueStrategy("escaped");
 
         $locationProvider.hashPrefix("");
+
+        cfpLoadingBarProvider.parentSelector = "#loading-bar-container";
+        cfpLoadingBarProvider.includeSpinner = true;
+        cfpLoadingBarProvider.latencyThreshold = 100;
 
         moment.locale(preferred);
     })
