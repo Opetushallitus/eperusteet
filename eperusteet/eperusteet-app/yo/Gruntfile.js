278b390d99a3c8e368d91f4cfda953abcb435812
Sauli Anto <sauli.anto@gofore.com>
2017-11-21 16:40:47 +0200

    HikariCp poistettu

--- eperusteet/eperusteet-app/yo/Gruntfile.js
+++ eperusteet/eperusteet-app/yo/Gruntfile.js
@@ -125,14 +125,14 @@ module.exports = function(grunt) {
       proxies: [{
         context: '/eperusteet-service',
         host: 'localhost',
-        port: 8080,
+        port: 8081,
         https: false,
         changeOrigin: false,
         xforward: false
       }, {
         context: '/virkailija-raamit',
         host: 'localhost',
-        port: 8080,
+        port: 8081,
         https: false,
         changeOrigin: false,
         xforward: false
