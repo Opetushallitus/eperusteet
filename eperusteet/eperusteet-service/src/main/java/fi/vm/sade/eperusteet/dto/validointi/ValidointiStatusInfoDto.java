d6b39e6669287ae064348d7f89b9067f39f3cbfc
Sauli Anto <sauli.anto@gmail.com>
2018-06-01 14:38:41 +0300

    Toteutettu testit kv-liitteelle

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/validointi/ValidointiStatusInfoDto.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/validointi/ValidointiStatusInfoDto.java
@@ -1,5 +1,6 @@
 package fi.vm.sade.eperusteet.dto.validointi;
 
+import fi.vm.sade.eperusteet.domain.Kieli;
 import fi.vm.sade.eperusteet.domain.Suoritustapakoodi;
 import fi.vm.sade.eperusteet.domain.TekstiPalanen;
 import lombok.Getter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Setter
 @Getter
@@ -15,4 +17,5 @@ public class ValidointiStatusInfoDto {
     private RakenneValidointiDto validointi;
     private List<TekstiPalanen> nimet = new ArrayList<>();
     private Suoritustapakoodi suoritustapa;
+    private Set<Kieli> kieli;
 }
