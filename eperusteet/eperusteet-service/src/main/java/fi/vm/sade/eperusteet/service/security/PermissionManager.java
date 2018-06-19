a700014eb7428474e89604e648a35e620a900437
Sauli Anto <sauli.anto@gmail.com>
2018-06-19 15:10:13 +0300

    Korjattu oikeuksien tarkastelu

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/security/PermissionManager.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/security/PermissionManager.java
@@ -32,7 +32,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
