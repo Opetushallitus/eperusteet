f7f799548f2671a7aa1bc048229efe8b7289a001
Sauli Anto <sauli.anto@gmail.com>
2019-01-08 10:53:56 +0200

    Lisätty moc request scope testeille

--- eperusteet/eperusteet-service/src/test/java/fi/vm/sade/eperusteet/service/test/AbstractIntegrationTest.java
+++ eperusteet/eperusteet-service/src/test/java/fi/vm/sade/eperusteet/service/test/AbstractIntegrationTest.java
@@ -17,12 +17,15 @@ package fi.vm.sade.eperusteet.service.test;
 
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  * Perusluokka service-tason integraatiotesteille joita ajetaan muistinvaraista kantaa vasten.
@@ -39,6 +42,10 @@ public class AbstractIntegrationTest {
         SecurityContext ctx = SecurityContextHolder.createEmptyContext();
         ctx.setAuthentication(new UsernamePasswordAuthenticationToken("test","test"));
         SecurityContextHolder.setContext(ctx);
+
+        // PerusteUpdateStoreImpl @scope:n takia
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
     }
 
 }
