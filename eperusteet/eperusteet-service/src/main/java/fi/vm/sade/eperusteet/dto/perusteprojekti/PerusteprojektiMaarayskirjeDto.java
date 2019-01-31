8b2c03a6d08c49f8881683cd077e684faac80b7b
Sauli Anto <sauli.anto@gmail.com>
2019-01-31 16:55:43 +0200

    Paranneltu ajastettuja tehtäviä

---
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/perusteprojekti/PerusteprojektiMaarayskirjeDto.java
@@ -0,0 +1,11 @@
+package fi.vm.sade.eperusteet.dto.perusteprojekti;
+
+import fi.vm.sade.eperusteet.dto.peruste.PerusteMaarayskirjeDto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PerusteprojektiMaarayskirjeDto {
+    private PerusteMaarayskirjeDto peruste;
+}
