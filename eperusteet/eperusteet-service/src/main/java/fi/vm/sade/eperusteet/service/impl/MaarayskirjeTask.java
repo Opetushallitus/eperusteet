86b351caca14fefa3fa20e153c32ebb1591d41fb
NJ <niko.junkala@gofore.com>
2019-03-04 15:25:42 +0200

    Poistettu taskit

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/impl/MaarayskirjeTask.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/impl/MaarayskirjeTask.java
@@ -5,7 +5,7 @@ import fi.vm.sade.eperusteet.service.ScheduledTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class MaarayskirjeTask implements ScheduledTask {
 
     @Autowired
