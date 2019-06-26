8d4d54c5f9bcc5908ef9f660da941ed8cfd6e583
Sauli Anto <sauli.anto@gmail.com>
2019-06-26 13:09:05 +0300

    Korjattu uuden lukion moduulin päivitys

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/domain/lops2019/oppiaineet/moduuli/Lops2019Moduuli.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/domain/lops2019/oppiaineet/moduuli/Lops2019Moduuli.java
@@ -17,38 +17,49 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
 @Entity
 @Audited
 @Table(name = "yl_lops2019_moduuli")
 public class Lops2019Moduuli extends AbstractAuditedReferenceableEntity {
 
+    @Getter
+    @Setter
     @NotNull
     @ValidHtml(whitelist = ValidHtml.WhitelistType.MINIMAL)
     @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
     private TekstiPalanen nimi;
 
+    @Getter
+    @Setter
     @ValidHtml(whitelist = ValidHtml.WhitelistType.NORMAL)
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
     private TekstiPalanen kuvaus;
 
+    @Getter
+    @Setter
     @NotNull
     private Boolean pakollinen;
 
+    @Getter
+    @Setter
     @Column(precision = 10, scale = 2)
     private BigDecimal laajuus;
 
+    @Getter
+    @Setter
     @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
     @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.ALL})
     private Koodi koodi;
 
+    @Getter
+    @Setter
     @JoinColumn(name = "tavoitteet_id")
     @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
     private Lops2019ModuuliTavoite tavoitteet;
 
+    @Getter
     @OneToMany(cascade = {CascadeType.ALL}, orphanRemoval = true)
     @JoinTable(name = "yl_lops2019_oppiaine_moduuli_sisalto",
             joinColumns = @JoinColumn(name = "moduuli_id"),
@@ -56,6 +67,8 @@ public class Lops2019Moduuli extends AbstractAuditedReferenceableEntity {
     @OrderBy("id")
     private List<Lops2019ModuuliSisalto> sisallot = new ArrayList<>();
 
+    @Getter
+    @Setter
     private Integer jarjestys;
 
     @Getter
@@ -66,4 +79,10 @@ public class Lops2019Moduuli extends AbstractAuditedReferenceableEntity {
             inverseJoinColumns = {@JoinColumn(name = "oppiaine_id", insertable = false, updatable = false)})
     private Lops2019Oppiaine oppiaine; // Moduuli viittaus oppiaineeseen/oppimäärään
 
+    public void setSisallot(List<Lops2019ModuuliSisalto> sisallot) {
+        this.sisallot.clear();
+        if (sisallot != null) {
+            this.sisallot.addAll(sisallot);
+        }
+    }
 }
