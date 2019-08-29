7df8a2f038b445b8b9a9d7c0884d76d8a0a3a6ac
Niko Junkala <niko.junkala@gofore.com>
2019-08-29 16:40:32 +0300

    Dokumenttien generointi pois testien aikana

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/dokumentti/impl/DokumenttiNewBuilderServiceImpl.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/dokumentti/impl/DokumenttiNewBuilderServiceImpl.java
@@ -29,6 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.NotImplementedException;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.w3c.dom.Document;
@@ -63,6 +64,7 @@ import static fi.vm.sade.eperusteet.service.dokumentti.impl.util.DokumenttiUtils
  */
 @Slf4j
 @Service
+@Profile("!test")
 public class DokumenttiNewBuilderServiceImpl implements DokumenttiNewBuilderService {
 
     private static final float COMPRESSION_LEVEL = 0.9f;
@@ -92,7 +94,6 @@ public class DokumenttiNewBuilderServiceImpl implements DokumenttiNewBuilderServ
     @Override
     public Document generateXML(Peruste peruste, Dokumentti dokumentti)
             throws ParserConfigurationException, IOException, TransformerException {
-
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
         Document doc = docBuilder.newDocument();
