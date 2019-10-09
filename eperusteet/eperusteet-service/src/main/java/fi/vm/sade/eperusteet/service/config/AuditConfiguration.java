8ade146317aa4058e9313b6b7e13667535d3e85e
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2019-10-09 11:47:11 +0300

    EP-1800

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/config/AuditConfiguration.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/config/AuditConfiguration.java
@@ -1,7 +1,8 @@
-package fi.vm.sade.eperusteet.service.audit;
+package fi.vm.sade.eperusteet.service.config;
 
 import fi.vm.sade.auditlog.ApplicationType;
 import fi.vm.sade.auditlog.Audit;
+import fi.vm.sade.eperusteet.service.audit.LoggerForAudit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
