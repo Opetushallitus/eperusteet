ab2dd0b819ef38d2a6880ba4da710f275058aa0a
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2019-12-09 12:35:35 +0200

    EP-1914

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/lops2019/Lops2019OppiaineKaikkiDto.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/lops2019/Lops2019OppiaineKaikkiDto.java
@@ -3,6 +3,7 @@ package fi.vm.sade.eperusteet.dto.lops2019;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import fi.vm.sade.eperusteet.dto.lops2019.oppiaineet.Lops2019OppiaineBaseDto;
 import fi.vm.sade.eperusteet.dto.lops2019.oppiaineet.moduuli.Lops2019ModuuliDto;
+import fi.vm.sade.eperusteet.dto.util.LokalisoituTekstiDto;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,8 @@ import java.util.List;
 @Getter
 @Setter
 public class Lops2019OppiaineKaikkiDto extends Lops2019OppiaineBaseDto {
+    private LokalisoituTekstiDto pakollisetModuulitKuvaus;
+    private LokalisoituTekstiDto valinnaisetModuulitKuvaus;
     private List<Lops2019ModuuliDto> moduulit = new ArrayList<>();
     private List<Lops2019OppiaineKaikkiDto> oppimaarat = new ArrayList<>();
 }
