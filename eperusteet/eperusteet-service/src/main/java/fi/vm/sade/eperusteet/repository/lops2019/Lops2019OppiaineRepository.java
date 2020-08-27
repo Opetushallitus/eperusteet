0eb4a2e9c0c4b099c146d4fee02f440df1055ff2
Sauli Anto <sauli.anto@gmail.com>
2020-08-27 14:52:41 +0300

    Korjattu lops2019 perusteen oppimäärien järjestys

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/repository/lops2019/Lops2019OppiaineRepository.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/repository/lops2019/Lops2019OppiaineRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Repository
 public interface Lops2019OppiaineRepository extends JpaWithVersioningRepository<Lops2019Oppiaine, Long> {
-    @Query("SELECT om FROM Lops2019Oppiaine om WHERE om.oppiaine IN (:oppiaineet)")
+    @Query("SELECT om FROM Lops2019Oppiaine om WHERE om.oppiaine IN (:oppiaineet) ORDER BY om.jarjestys")
     List<Lops2019Oppiaine> getOppimaaratByParents(@Param("oppiaineet") Collection<Lops2019Oppiaine> oppiaineet);
 
 }
