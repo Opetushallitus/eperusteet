7781ff53174cc73ebb38bab78f637827921fabc0
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-10-09 10:44:10 +0300

    EP-2429-spec-update

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/VapaasivistystyoSisaltoService.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/VapaasivistystyoSisaltoService.java
@@ -1,11 +1,11 @@
 package fi.vm.sade.eperusteet.service;
 
-import fi.vm.sade.eperusteet.dto.vst.KevytVapaasivistystyoSisaltoDto;
+import fi.vm.sade.eperusteet.dto.vst.VapaasivistystyoSisaltoKevytDto;
 import fi.vm.sade.eperusteet.dto.vst.VapaasivistystyoSisaltoDto;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface VapaasivistystyoSisaltoService {
 
     @PreAuthorize("hasPermission(#perusteId, 'peruste', 'MUOKKAUS')")
-    VapaasivistystyoSisaltoDto update(Long perusteId, KevytVapaasivistystyoSisaltoDto vapaasivistystyoSisaltoDto);
+    VapaasivistystyoSisaltoDto update(Long perusteId, VapaasivistystyoSisaltoKevytDto vapaasivistystyoSisaltoDto);
 }
