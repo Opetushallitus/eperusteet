e51f0968bf6b728f266637db9d863dda7900c650
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-02-25 11:43:50 +0200

    EP-2647

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/util/PerusteenRakenne.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/util/PerusteenRakenne.java
@@ -259,6 +259,14 @@ public class PerusteenRakenne {
                     validointi.ongelmat.add(new Ongelma("Laskettu koko on pienempi kuin ryhmän vaadittu maksimi (" + osat.size() + " < " + kokoMax + ").", nimi, syvyys));
                 }
                 validointi.laskettuLaajuus = ms.laajuusMaksimi() != null && ms.laajuusMaksimi() > 0 ? laajuusMax : laajuusSummaMax;
+            } else if (rooli == RakenneModuuliRooli.VIRTUAALINEN) {
+                if (laajuusMax.compareTo(BigDecimal.valueOf(Integer.MIN_VALUE)) != 0) {
+                    validointi.laskettuLaajuus = laajuusMax;
+                } else if (laajuusMin.compareTo(BigDecimal.valueOf(Integer.MIN_VALUE)) != 0) {
+                    validointi.laskettuLaajuus = laajuusMin;
+                } else {
+                    validointi.laskettuLaajuus = laajuusMax;
+                }
             } else {
                 validointi.laskettuLaajuus = laajuusMax;
             }
