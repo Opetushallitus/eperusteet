e88d17e519eb7df97d7a925db946822467ba26ee
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2022-02-01 09:00:54 +0200

    EP-2941: ajettu koodi formatterin läpi

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/domain/validation/ValidHtml.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/domain/validation/ValidHtml.java
@@ -27,42 +27,43 @@ import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target({ FIELD, ANNOTATION_TYPE })
+@Target({FIELD, ANNOTATION_TYPE})
 @Retention(RUNTIME)
 @Constraint(validatedBy = {ValidHtmlValidator.class, ValidHtmlCollectionValidator.class})
 @Documented
 public @interface ValidHtml {
 
-	String message() default "Teksti saa sisältää vain ennaltamääriteltyjä html-elementtejä";
-	WhitelistType whitelist() default WhitelistType.NORMAL;
+    String message() default "Teksti saa sisältää vain ennaltamääriteltyjä html-elementtejä";
+
+    WhitelistType whitelist() default WhitelistType.NORMAL;
 
     Class<?>[] groups() default {};
 
-    Class<? extends Payload>[] payload() default { };
+    Class<? extends Payload>[] payload() default {};
 
     enum WhitelistType {
-    	MINIMAL(Whitelist.none()),
-		SIMPLIFIED(Whitelist.none().addTags("p", "strong", "em", "i", "s", "ol", "li", "ul")),
-		NORMAL(Whitelist.none()
-				.addTags("p", "span", "strong", "em", "i", "s", "ol", "li", "ul", "blockquote", "table", "caption",
-						"tbody", "tr", "td", "hr", "pre", "th", "thead", "a", "abbr", "comment", "figcaption", "br")
-				.addAttributes("table", "align", "border", "cellpadding", "cellspacing", "style", "summary")
-				.addAttributes("th", "scope", "colspan", "rowspan", "style")
-				.addAttributes("td", "colspan", "rowspan", "style", "data-colwidth")
-				.addAttributes("a", "href", "target", "rel", "routenode")
-				.addAttributes("img", "data-uid", "src", "alt", "height", "width", "style", "figcaption")
-				.addAttributes("abbr", "data-viite")
-				.addAttributes("figure", "class")
-				.addAttributes("span", "kommentti", "class"));
+        MINIMAL(Whitelist.none()),
+        SIMPLIFIED(Whitelist.none().addTags("p", "strong", "em", "i", "s", "ol", "li", "ul")),
+        NORMAL(Whitelist.none()
+                .addTags("p", "span", "strong", "em", "i", "s", "ol", "li", "ul", "blockquote", "table", "caption",
+                        "tbody", "tr", "td", "hr", "pre", "th", "thead", "a", "abbr", "comment", "figcaption", "br")
+                .addAttributes("table", "align", "border", "cellpadding", "cellspacing", "style", "summary")
+                .addAttributes("th", "scope", "colspan", "rowspan", "style")
+                .addAttributes("td", "colspan", "rowspan", "style", "data-colwidth")
+                .addAttributes("a", "href", "target", "rel", "routenode")
+                .addAttributes("img", "data-uid", "src", "alt", "height", "width", "style", "figcaption")
+                .addAttributes("abbr", "data-viite")
+                .addAttributes("figure", "class")
+                .addAttributes("span", "kommentti", "class"));
 
-    	private final Whitelist whitelist;
+        private final Whitelist whitelist;
 
-    	WhitelistType(Whitelist whitelist) {
-    		this.whitelist = whitelist;
-    	}
+        WhitelistType(Whitelist whitelist) {
+            this.whitelist = whitelist;
+        }
 
-    	public Whitelist getWhitelist() {
-    		return whitelist;
-    	}
+        public Whitelist getWhitelist() {
+            return whitelist;
+        }
     }
 }
