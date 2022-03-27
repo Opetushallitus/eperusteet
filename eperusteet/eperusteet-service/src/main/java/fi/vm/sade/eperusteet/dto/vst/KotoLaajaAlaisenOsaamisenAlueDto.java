8f9388daac15a6156d6bf3268d97a37226f19cfe
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2022-03-27 20:50:37 +0300

    EP-3075: Konstruktori KotoLaajaAlaisenOsaamisenAlueDto:lle

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/vst/KotoLaajaAlaisenOsaamisenAlueDto.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/vst/KotoLaajaAlaisenOsaamisenAlueDto.java
@@ -2,11 +2,15 @@ package fi.vm.sade.eperusteet.dto.vst;
 
 import fi.vm.sade.eperusteet.dto.tutkinnonrakenne.KoodiDto;
 import fi.vm.sade.eperusteet.dto.util.LokalisoituTekstiDto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class KotoLaajaAlaisenOsaamisenAlueDto {
 
     private KoodiDto koodi;
