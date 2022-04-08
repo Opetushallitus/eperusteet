4f61ec192dbf533a865b62974ad9a01c2ca8bb66
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2022-04-08 11:19:26 +0300

    EP-2999-3

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/peruste/PerusteBaseDto.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/peruste/PerusteBaseDto.java
@@ -31,6 +31,7 @@ import fi.vm.sade.eperusteet.dto.util.LokalisoituTekstiDto;
 import fi.vm.sade.eperusteet.dto.vst.VapaasivistystyoSisaltoDto;
 import fi.vm.sade.eperusteet.service.util.PerusteIdentifiable;
 import fi.vm.sade.eperusteet.service.util.PerusteUtils;
+import io.swagger.annotations.ApiModelProperty;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
@@ -50,10 +51,14 @@ public abstract class PerusteBaseDto implements Serializable, PerusteIdentifiabl
     private Long id;
     @JsonIgnore
     private Integer revision;
+
+    @ApiModelProperty("Perusteen sisältöjen viimeisin päivitysaika")
     private PerusteVersionDto globalVersion;
 
     private LokalisoituTekstiDto nimi;
     private String koulutustyyppi;
+
+    @ApiModelProperty("Perusteen sisäisen rakenteen toteutuksen ilmaiseva sisältö. Esimerkiksi vanhan ja uuden lukion toteutus (LOPS vs LOPS2019)")
     private KoulutustyyppiToteutus toteutus;
     private Set<KoulutusDto> koulutukset;
     private Set<Kieli> kielet;
@@ -63,34 +68,53 @@ public abstract class PerusteBaseDto implements Serializable, PerusteIdentifiabl
 
     private String diaarinumero;
     private Date voimassaoloAlkaa;
+
+    @ApiModelProperty("Voimassaolon loppumisen jälkeinen perusteen päättymispäivämäärä.")
     private Date siirtymaPaattyy;
     private Date voimassaoloLoppuu;
+
+    @ApiModelProperty("Perusteen määräyksen päätöspäivämäärä")
     private Date paatospvm;
 
+    @ApiModelProperty(hidden = true)
     private Date luotu;
+    @ApiModelProperty(hidden = true)
     private Date muokattu;
+
+    @ApiModelProperty("Perusteen sisäinen tila. Ei enää merkityksellinen julkaisujen käytönoton jälkeen")
     private PerusteTila tila;
     private PerusteTyyppi tyyppi;
+
+    @ApiModelProperty("EU- ja ETA-maiden koulutusvientikokeiluun tarkoitettu peruste")
     private Boolean koulutusvienti;
 
+    @ApiModelProperty("Perusteen vanhemmat määräykset. Eivät välttämättä ole toteutettu ePerusteisiin")
     private Set<String> korvattavatDiaarinumerot;
+
+    @ApiModelProperty("Perusteeseen liittyvät osaamisalakoodit")
     private Set<KoodiDto> osaamisalat;
 
     // Tuodaan kvliitteestä
+    @ApiModelProperty("kv-liitteen lisätieto")
     private LokalisoituTekstiDto tyotehtavatJoissaVoiToimia;
+
+    @ApiModelProperty("kv-liitteen lisätieto")
     private LokalisoituTekstiDto suorittaneenOsaaminen;
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     List<TutkintonimikeKoodiDto> tutkintonimikkeet;
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    @ApiModelProperty("Perusteet joihin opas liittyy")
     private Set<PerusteKevytDto> oppaanPerusteet;
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    @ApiModelProperty("Koulutustyypit joihin opas liittyy")
     private Set<KoulutusTyyppi> oppaanKoulutustyypit;
 
+    @ApiModelProperty("Perusteprosessin päivämäärät")
     private Set<PerusteAikatauluDto> perusteenAikataulut;
-    
+
     @Override
     public KoulutustyyppiToteutus getToteutus() {
         return PerusteUtils.getToteutus(this.toteutus, this.koulutustyyppi, this.tyyppi);
