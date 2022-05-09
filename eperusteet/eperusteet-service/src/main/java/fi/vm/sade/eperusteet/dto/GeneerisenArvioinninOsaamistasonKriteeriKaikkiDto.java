b9d05c683acbcc656ebdcbe92b0b62b8a78fe60e
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2022-05-09 09:07:48 +0300

    EP-3144

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/GeneerisenArvioinninOsaamistasonKriteeriKaikkiDto.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/GeneerisenArvioinninOsaamistasonKriteeriKaikkiDto.java
@@ -7,14 +7,12 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@EqualsAndHashCode(of = "osaamistaso")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
