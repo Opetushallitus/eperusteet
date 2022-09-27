4afe1ebcf5bfdcb87b459be5d71d37a4b9f234fa
Timo Paasila <timo.paasila@gofore.com>
2022-09-27 10:01:40 +0300

    EP-3318 - uusi ValidatorUtil-luokka ja validoinnit tutkintonimikkeille

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/util/ValidatorUtil.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/util/ValidatorUtil.java
@@ -2,14 +2,12 @@ package fi.vm.sade.eperusteet.service.util;
 
 import fi.vm.sade.eperusteet.domain.Peruste;
 import fi.vm.sade.eperusteet.dto.peruste.TutkintonimikeKoodiDto;
+import lombok.experimental.UtilityClass;
 import org.apache.commons.lang.StringUtils;
 
 import java.util.List;
-
+@UtilityClass
 public class ValidatorUtil {
-
-    public ValidatorUtil() {
-    }
 
     public static boolean hasValidTutkintonimikkeet(Peruste peruste, List<TutkintonimikeKoodiDto> tutkintonimikeKoodiDtos) {
         for (TutkintonimikeKoodiDto tutkintonimike : tutkintonimikeKoodiDtos) {
