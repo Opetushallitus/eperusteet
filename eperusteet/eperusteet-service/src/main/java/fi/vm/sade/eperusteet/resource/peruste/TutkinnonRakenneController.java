ee010350624b41fdd1b57939359b9d6db54f8fd5
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-02-02 16:13:20 +0200

    EP-3554

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/resource/peruste/TutkinnonRakenneController.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/resource/peruste/TutkinnonRakenneController.java
@@ -34,8 +34,6 @@ import fi.vm.sade.eperusteet.service.KayttajanTietoService;
 import fi.vm.sade.eperusteet.service.PerusteService;
 import fi.vm.sade.eperusteet.service.PerusteenOsaViiteService;
 import io.swagger.annotations.Api;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -46,6 +44,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static fi.vm.sade.eperusteet.resource.util.Etags.eTagHeader;
 import static fi.vm.sade.eperusteet.resource.util.Etags.revisionOf;
@@ -294,5 +295,4 @@ public class TutkinnonRakenneController {
             @PathVariable("koodiUri") final String koodiUri) {
         return perusteService.getTutkinnonOsaViiteByKoodiUri(id, suoritustapakoodi, koodiUri);
     }
-    
 }
