05037ff2e46fd5521ad64843af532f005c9d7d35
timopaasila <113356513+timopaasila@users.noreply.github.com>
2023-05-30 13:38:23 +0300

    EP-3751 (#1190)
    EP-3751

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/domain/JarjestysTapa.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/domain/JarjestysTapa.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (c) 2013 The Finnish Board of Education - Opetushallitus
- *
- * This program is free software: Licensed under the EUPL, Version 1.1 or - as
- * soon as they will be approved by the European Commission - subsequent versions
- * of the EUPL (the "Licence");
- *
- * You may not use this work except in compliance with the Licence.
- * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * European Union Public Licence for more details.
- */
 package fi.vm.sade.eperusteet.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -24,6 +9,7 @@ public enum JarjestysTapa {
     MUOKATTU("muokattu"),
     PERUSTE_VOIMASSAOLO_ALKAA("peruste.voimassaoloAlkaa"),
     PERUSTE_VOIMASSAOLO_LOPPUU("peruste.voimassaoloLoppuu"),
+    PERUSTE_PAATOSPVM("peruste.paatospvm"),
     KOULUTUSTYYPPI("koulutustyyppi");
 
     private final String tapa;
