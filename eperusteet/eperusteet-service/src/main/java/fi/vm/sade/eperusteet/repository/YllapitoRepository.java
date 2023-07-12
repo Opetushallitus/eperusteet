0330f13d6383e5c2c2d6b0bd33548c2658018a0d
Timo Paasila <timo.paasila@gofore.com>
2023-07-12 09:59:14 +0300

    EP-3817

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/repository/YllapitoRepository.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/repository/YllapitoRepository.java
@@ -2,11 +2,9 @@ package fi.vm.sade.eperusteet.repository;
 
 import fi.vm.sade.eperusteet.domain.Yllapito;
 import fi.vm.sade.eperusteet.repository.version.JpaWithVersioningRepository;
-import java.util.List;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface YllapitoRepository extends JpaWithVersioningRepository<Yllapito, Long> {
-
-    List<Yllapito> findBySallittu(boolean sallittu);
+    Yllapito findById(Long id);
 }
