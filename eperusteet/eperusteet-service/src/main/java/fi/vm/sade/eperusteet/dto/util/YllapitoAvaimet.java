5be23e50bfb63d612fdd041cd5660fc1dd08fe37
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-08-01 14:54:15 +0300

    EP-3817-2

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/util/YllapitoAvaimet.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/util/YllapitoAvaimet.java
@@ -4,6 +4,7 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class YllapitoAvaimet {
-
     public static final String USE_PDF_SERVICE_EPERUSTEET = "use-pdf-service-eperusteet";
+    public static final String USE_PDF_SERVICE_YLOPS = "use-pdf-service-ylops";
+    public static final String USE_PDF_SERVICE_AMOSAA = "use-pdf-service-amosaa";
 }
