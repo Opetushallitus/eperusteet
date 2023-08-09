f460b3819abadbb9e809ad47ca4490dee8a678ae
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-08-09 12:06:52 +0300

    EP-3718

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/dokumentti/impl/ExternalPdfServiceImpl.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/dokumentti/impl/ExternalPdfServiceImpl.java
@@ -1,6 +1,7 @@
 package fi.vm.sade.eperusteet.service.dokumentti.impl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fi.vm.sade.eperusteet.domain.GeneratorVersion;
 import fi.vm.sade.eperusteet.dto.DokumenttiDto;
@@ -8,6 +9,8 @@ import fi.vm.sade.eperusteet.dto.peruste.PerusteKaikkiDto;
 import fi.vm.sade.eperusteet.resource.config.InitJacksonConverter;
 import fi.vm.sade.eperusteet.service.PerusteService;
 import fi.vm.sade.eperusteet.service.dokumentti.ExternalPdfService;
+import fi.vm.sade.eperusteet.service.exception.BusinessRuleViolationException;
+import fi.vm.sade.eperusteet.service.mapping.KayttajanTietoParser;
 import fi.vm.sade.eperusteet.utils.client.RestClientFactory;
 import fi.vm.sade.javautils.http.OphHttpClient;
 import fi.vm.sade.javautils.http.OphHttpEntity;
@@ -16,6 +19,11 @@ import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Optional;
 
 import static javax.servlet.http.HttpServletResponse.SC_ACCEPTED;
 import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
@@ -24,6 +32,7 @@ import static javax.servlet.http.HttpServletResponse.SC_FOUND;
 import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 import static javax.servlet.http.HttpServletResponse.SC_METHOD_NOT_ALLOWED;
 import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
 
 @Service
 public class ExternalPdfServiceImpl implements ExternalPdfService {
@@ -50,7 +59,7 @@ public class ExternalPdfServiceImpl implements ExternalPdfService {
             url = url + "/kvliite";
         }
 
-        client.execute(
+        String result = (String) client.execute(
                 OphHttpRequest.Builder
                     .post(url)
                     .addHeader("Content-Type", "application/json;charset=UTF-8")
@@ -59,10 +68,14 @@ public class ExternalPdfServiceImpl implements ExternalPdfService {
                             .contentType(ContentType.APPLICATION_JSON)
                             .build())
                     .build())
-                .handleErrorStatus(SC_FOUND, SC_UNAUTHORIZED, SC_FORBIDDEN, SC_METHOD_NOT_ALLOWED, SC_BAD_REQUEST, SC_INTERNAL_SERVER_ERROR)
-                .with(error -> {
-                    throw new RuntimeException("Virhe pdf:n luonnissa: " + error);
-                })
-                .expectedStatus(SC_ACCEPTED);
+                .handleErrorStatus(SC_FOUND, SC_UNAUTHORIZED, SC_FORBIDDEN, SC_METHOD_NOT_ALLOWED, SC_BAD_REQUEST, SC_INTERNAL_SERVER_ERROR, SC_NOT_FOUND)
+                .with(res -> Optional.of("error"))
+                .expectedStatus(SC_ACCEPTED)
+                .mapWith(res -> res)
+                .orElse(null);
+
+        if (!ObjectUtils.isEmpty(result)) {
+            throw new RuntimeException("Virhe pdf-palvelun kutsussa");
+        }
     }
 }
