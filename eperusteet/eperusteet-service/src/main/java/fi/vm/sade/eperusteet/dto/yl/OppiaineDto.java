64353e23c45733831f1dbdb21c7628fbc249b97d
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-09-05 13:32:16 +0300

    EP-3870

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/yl/OppiaineDto.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/yl/OppiaineDto.java
@@ -47,10 +47,10 @@ public class OppiaineDto extends OppiaineBaseUpdateDto {
     private List<KevytTekstiKappaleDto> vapaatTekstit;
 
     public Optional<LokalisoituTekstiDto> getNimi() {
-        if (koodi != null) {
-            return Optional.of(koodi.getNimi());
+        if (super.getNimi().isPresent()) {
+            return super.getNimi();
         }
 
-        return super.getNimi();
+        return Optional.of(koodi).map(KoodiDto::getNimi);
     }
 }
