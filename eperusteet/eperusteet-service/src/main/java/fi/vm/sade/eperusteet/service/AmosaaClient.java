2fb35fb63fe05df3e72ad3a24f2cd976a7b05675
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-09-12 14:11:19 +0300

    EP-3867

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/AmosaaClient.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/AmosaaClient.java
@@ -1,11 +1,14 @@
 package fi.vm.sade.eperusteet.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import java.util.List;
 import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.List;
 
 public interface AmosaaClient {
 
     @PreAuthorize("hasPermission(null, 'perusteprojekti', 'LUONTI')")
     List<Object> getTilastot();
+
+    @PreAuthorize("hasPermission(null, 'arviointiasteikko', 'MUOKKAUS')")
+    void updateArvioinnit();
 }
