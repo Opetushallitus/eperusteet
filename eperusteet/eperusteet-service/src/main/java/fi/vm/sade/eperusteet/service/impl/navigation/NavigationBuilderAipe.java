261c7c8ebee9b2d35b874ee1a6000be19c509b90
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-09-22 11:18:28 +0300

    EP-3897

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/impl/navigation/NavigationBuilderAipe.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/impl/navigation/NavigationBuilderAipe.java
@@ -23,23 +23,26 @@ public class NavigationBuilderAipe {
     public List<NavigationNodeDto> buildNavigation(Long perusteId, String kieli) {
         return aipeOpetuksenPerusteenSisaltoService.getVaiheetKaikki(perusteId).stream()
                 .map(vaihe -> NavigationNodeDto.of(NavigationType.aipevaihe, vaihe.getNimi() != null ? vaihe.getNimi().orElse(null) : null, vaihe.getId())
-                        .addAll(oppiaineet(vaihe.getOppiaineet(), kieli)))
+                        .addAll(oppiaineet(vaihe.getOppiaineet(), vaihe.getId(), kieli)))
                 .collect(Collectors.toList());
     }
 
-    private List<NavigationNodeDto> oppiaineet(List<AIPEOppiaineLaajaDto> oppiaineet, String kieli) {
+    private List<NavigationNodeDto> oppiaineet(List<AIPEOppiaineLaajaDto> oppiaineet, Long vaiheId, String kieli) {
         return oppiaineet.stream()
                 .map(oppiaine -> NavigationNodeDto
                         .of(NavigationType.aipeoppiaine, oppiaine.getNimi() != null ? oppiaine.getNimi().orElse(null) : null, oppiaine.getId())
-                        .addAll(oppiaineet(oppiaine.getOppimaarat(), kieli))
-                        .addAll(kurssit(oppiaine.getKurssit(), kieli)))
+                        .meta("vaiheId", vaiheId)
+                        .addAll(oppiaineet(oppiaine.getOppimaarat(), vaiheId, kieli))
+                        .addAll(kurssit(oppiaine.getKurssit(), vaiheId, oppiaine.getId(), kieli)))
                 .collect(Collectors.toList());
     }
 
-    private List<NavigationNodeDto> kurssit(List<AIPEKurssiDto> kurssit, String kieli) {
+    private List<NavigationNodeDto> kurssit(List<AIPEKurssiDto> kurssit, Long vaiheId, Long oppiaineId, String kieli) {
         return kurssit.stream()
                 .map(kurssi -> NavigationNodeDto
                         .of(NavigationType.aipekurssi, kurssi.getNimi() != null ? kurssi.getNimi().orElse(null) : null, kurssi.getId())
+                        .meta("vaiheId", vaiheId)
+                        .meta("oppiaineId", oppiaineId)
                 )
                 .collect(Collectors.toList());
     }
