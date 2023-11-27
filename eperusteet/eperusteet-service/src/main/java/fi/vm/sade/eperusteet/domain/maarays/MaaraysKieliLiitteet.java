828ce78a81083e7960eb1608abe3f0961b00eddc
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-11-27 16:35:17 +0200

    EP-3782-2

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/domain/maarays/MaaraysKieliLiitteet.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/domain/maarays/MaaraysKieliLiitteet.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -27,7 +28,7 @@ public class MaaraysKieliLiitteet {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @OneToMany()
+    @OneToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "maarays_kieli_liite")
     private List<MaaraysLiite> liitteet = new ArrayList<>();
 }
