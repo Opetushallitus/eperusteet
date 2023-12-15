8988c076463982b202177f7b4ab1e2c1a58cd321
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-12-15 14:41:43 +0200

    EP-4024

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/yl/OppiaineLaajaDto.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/dto/yl/OppiaineLaajaDto.java
@@ -16,11 +16,13 @@
 package fi.vm.sade.eperusteet.dto.yl;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import fi.vm.sade.eperusteet.dto.KevytTekstiKappaleDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -34,6 +36,7 @@ import java.util.Set;
 @AllArgsConstructor
 public class OppiaineLaajaDto extends OppiaineBaseDto {
     private Optional<TekstiOsaDto> tehtava;
+    private List<KevytTekstiKappaleDto> vapaatTekstit;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private Set<OppiaineDto> oppimaarat;
     private Set<OpetuksenKohdealueDto> kohdealueet;
