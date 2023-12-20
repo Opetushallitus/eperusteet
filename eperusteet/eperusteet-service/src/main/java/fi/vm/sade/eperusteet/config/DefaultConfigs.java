e3dfcb9e8d977fa800014856ac376c707b25c61e
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-12-20 12:50:40 +0200

    cache fix

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/config/DefaultConfigs.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/config/DefaultConfigs.java
@@ -1,6 +1,8 @@
 package fi.vm.sade.eperusteet.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
