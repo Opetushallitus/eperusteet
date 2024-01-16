0948530576a78607da0e4c0b4be545c17471a4fd
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-01-16 15:24:57 +0200

    hibernate cache fix

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/config/DefaultConfigs.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/config/DefaultConfigs.java
@@ -1,14 +1,11 @@
 package fi.vm.sade.eperusteet.config;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.security.cas.authentication.CasAuthenticationProvider;
 import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
 
 @Configuration
