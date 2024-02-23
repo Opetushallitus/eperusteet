8d2be6eaf9c98be41005b0aaccf70b93777f07f8
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-02-23 11:11:01 +0200

    interceptor fix

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/hibernate/MetadataIntegrator.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/hibernate/MetadataIntegrator.java
@@ -17,7 +17,13 @@
 package fi.vm.sade.eperusteet.hibernate;
 
 import fi.ratamaa.dtoconverter.reflection.Property;
+import fi.vm.sade.eperusteet.domain.annotation.RelatesToPeruste;
+import org.hibernate.boot.Metadata;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.integrator.spi.Integrator;
+import org.hibernate.service.spi.SessionFactoryServiceRegistry;
 
+import javax.persistence.Transient;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -28,39 +34,35 @@ import java.util.Map;
  * Date: 27.11.2015
  * Time: 16.24
  */
-public class MetadataIntegrator { //implements Integrator {
+public class MetadataIntegrator implements Integrator {
     private static Map<Class<?>, List<Property>> anywhareReferencedFromProperties = new HashMap<>();
 
-//    @Override
-//    public void integrate(Configuration configuration, SessionFactoryImplementor sessionFactory, SessionFactoryServiceRegistry serviceRegistry) {
-//        configuration.getClassMappings().forEachRemaining(
-//                mapping -> Property.mapForClass(mapping.getMappedClass())
-//                        .values().stream().filter(p -> p.getContainedTypeOrType()
-//                                .isAnnotationPresent(RelatesToPeruste.FromAnywhereReferenced.class)
-//                            && !p.isAnnotationPresent(Transient.class))
-//                        .forEach(p -> classPropertyList(p.getContainedTypeOrType()).add(p)));
-//    }
-//
-//    private List<Property> classPropertyList(Class<?> clz) {
-//        List<Property> list = anywhareReferencedFromProperties.get(clz);
-//        if (list == null) {
-//            list = new ArrayList<>();
-//            anywhareReferencedFromProperties.put(clz, list);
-//        }
-//        return list;
-//    }
+    @Override
+    public void integrate(Metadata metadata, SessionFactoryImplementor sessionFactory, SessionFactoryServiceRegistry serviceRegistry) {
+        metadata.getEntityBindings().forEach(
+                mapping -> Property.mapForClass(mapping.getMappedClass())
+                        .values().stream().filter(p -> p.getContainedTypeOrType()
+                                .isAnnotationPresent(RelatesToPeruste.FromAnywhereReferenced.class)
+                                && !p.isAnnotationPresent(Transient.class))
+                        .forEach(p -> classPropertyList(p.getContainedTypeOrType()).add(p)));
+    }
+
+    private List<Property> classPropertyList(Class<?> clz) {
+        List<Property> list = anywhareReferencedFromProperties.get(clz);
+        if (list == null) {
+            list = new ArrayList<>();
+            anywhareReferencedFromProperties.put(clz, list);
+        }
+        return list;
+    }
 
     public static List<Property> findPropertiesReferencingTo(Class<?> clz) {
         List<Property> list = anywhareReferencedFromProperties.get(clz);
         return list == null ? new ArrayList<>() : list;
     }
 
-//    @Override
-//    public void integrate(MetadataImplementor metadata, SessionFactoryImplementor sessionFactory, SessionFactoryServiceRegistry serviceRegistry) {
-//    }
-
-//    @Override
-//    public void disintegrate(SessionFactoryImplementor sessionFactory, SessionFactoryServiceRegistry serviceRegistry) {
-//        anywhareReferencedFromProperties = new HashMap<>();
-//    }
+    @Override
+    public void disintegrate(SessionFactoryImplementor sessionFactory, SessionFactoryServiceRegistry serviceRegistry) {
+        anywhareReferencedFromProperties = new HashMap<>();
+    }
 }
