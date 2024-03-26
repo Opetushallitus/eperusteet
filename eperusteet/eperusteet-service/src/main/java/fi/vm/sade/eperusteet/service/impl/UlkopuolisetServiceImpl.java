d944b96e6c77ba17a5b124e9d85bcd0bc799351c
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-03-26 10:30:15 +0200

    EP-4180-2

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/impl/UlkopuolisetServiceImpl.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/impl/UlkopuolisetServiceImpl.java
@@ -45,6 +45,9 @@ public class UlkopuolisetServiceImpl implements UlkopuolisetService {
     @Value("${cas.service.organisaatio-service:''}")
     private String serviceUrl;
 
+    @Value("${cas.service.organisaatio-service.internal:${cas.service.organisaatio-service:''}}")
+    private String serviceInternalUrl;
+
     private static final String ORGANISAATIOT = "/rest/organisaatio/";
     private static final String ORGANISAATIORYHMAT = ORGANISAATIOT + "1.2.246.562.10.00000000001/ryhmat";
 
@@ -59,7 +62,7 @@ public class UlkopuolisetServiceImpl implements UlkopuolisetService {
 
         OphHttpClient client = restClientFactory.get(serviceUrl, true);
 
-        String url = serviceUrl + ORGANISAATIOT + organisaatioOid;
+        String url = serviceInternalUrl + ORGANISAATIOT + organisaatioOid;
 
         OphHttpRequest request = OphHttpRequest.Builder
                 .get(url)
@@ -85,7 +88,7 @@ public class UlkopuolisetServiceImpl implements UlkopuolisetService {
 
         OphHttpClient client = restClientFactory.get(serviceUrl, true);
 
-        String url = serviceUrl + ORGANISAATIORYHMAT;
+        String url = serviceInternalUrl + ORGANISAATIORYHMAT;
 
         OphHttpRequest request = OphHttpRequest.Builder
                 .get(url)
