33c0580cd8f5dcdcd911ef4746132e6dbe3db9da
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-03-27 14:54:10 +0200

    auditlogger back

---
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/audit/EperusteetOperation.java
@@ -0,0 +1,17 @@
+package fi.vm.sade.eperusteet.service.audit;
+
+public enum EperusteetOperation {
+    OIKEUSLISAYS,
+    JARJESTA,
+    LUONTI,
+    LIITOS,
+    LISAYS,
+    MUOKKAUS,
+    POISTO,
+    KLOONAUS,
+    PALAUTUS,
+    GENEROI,
+    TILAMUUTOS,
+    LUKITUS,
+    LUKITUKSENVAPAUTUS,
+}
