9f9759d72ca4fcefad220136530a9de48ad1ffa3
Timo Paasila <timo.paasila@gofore.com>
2024-04-18 09:34:26 +0300

    EP-4102

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/config/SwaggerConfig.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/config/SwaggerConfig.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (c) 2013 The Finnish Board of Education - Opetushallitus
- *
- * This program is free software: Licensed under the EUPL, Version 1.1 or - as
- * soon as they will be approved by the European Commission - subsequent versions
- * of the EUPL (the "Licence");
- *
- * You may not use this work except in compliance with the Licence.
- * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * European Union Public Licence for more details.
- */
 package fi.vm.sade.eperusteet.config;
 
 import com.fasterxml.classmate.GenericType;
@@ -26,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
@@ -42,10 +26,6 @@ import java.util.concurrent.Callable;
 
 import static com.google.common.base.Predicates.not;
 
-/**
- *
- * @author jhyoty
- */
 @Configuration
 @EnableSwagger
 @EnableSwagger2
