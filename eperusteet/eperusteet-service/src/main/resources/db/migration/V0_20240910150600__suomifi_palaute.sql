edd5ed4b6d6946fe225e67da374ac86150e2d4d1
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-09-11 10:54:02 +0300

    EP-3012

---
+++ eperusteet/eperusteet-service/src/main/resources/db/migration/V0_20240910150600__suomifi_palaute.sql
@@ -0,0 +1,4 @@
+INSERT INTO yllapito(id, kuvaus, key, value) VALUES (1, 'Osoite, johon perusteiden laadinnasta ohjataan käyttäjä antamaan palaute', 'perusteiden-laadinta-palaute-url', '');
+INSERT INTO yllapito(id, kuvaus, key, value) VALUES (2, 'Osoite, johon Ops-työkalusta ohjataan käyttäjä antamaan palaute', 'ops-tyokalu-palaute-url', '');
+INSERT INTO yllapito(id, kuvaus, key, value) VALUES (3, 'Osoite, johon Amosaan käyttöliittymistä ohjataan käyttäjä antamaan palaute', 'amosaa-tyokalu-palaute-url', '');
+INSERT INTO yllapito(id, kuvaus, key, value) VALUES (4, 'Osoite, johon julkisivulta ohjataan käyttäjä antamaan palaute', 'eperusteet-opintopolku-palaute-url', '');
