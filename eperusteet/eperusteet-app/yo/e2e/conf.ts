cba32ea7c3ee794fff6e685a4b915c7dc81fa66e
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-11-18 09:19:09 +0200

    EP-4475

--- eperusteet/eperusteet-app/yo/e2e/conf.ts
+++
@@ -1,9 +0,0 @@
-declare var exports: any;
-
-exports.config = {
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['e2e/result.js'],
-    jasmineNodeOpts: {
-        showColors: true
-    }
-};
--- eperusteet/eperusteet-app/yo/e2e/protractor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-protractor="./node_modules/protractor"
-tsc conf.ts
-tsc --outFile e2e/result.js
-$protractor/bin/webdriver-manager start 2> /dev/null &
-sleep 2
-$protractor/bin/protractor conf.js
-kill $(lsof -i :4444|awk '{print $2}'|tail -n 1)
-wait
