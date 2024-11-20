7ba1bdb3c54f1ea46a1c00ed7c86f2d119a779c8
Olli-Pekka Lehtokallio <53811784+oplekal@users.noreply.github.com>
2024-11-20 10:11:22 +0200

    EP-4475 (#1535)

--- eperusteet/eperusteet-app/yo/e2e/typings.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="../typings/jasmine/jasmine.d.ts" />
-/// <reference path="../typings/angular-protractor/angular-protractor.d.ts" />
-/// <reference path="../typings/selenium-webdriver/selenium-webdriver.d.ts" />
-/// <reference path="../typings/jasmine/jasmine.d.ts" />
