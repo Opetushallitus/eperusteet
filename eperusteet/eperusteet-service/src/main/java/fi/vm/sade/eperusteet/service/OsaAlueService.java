0d4c7814dd7c0b5aea4dc3b4c6c67cc63b03ec51
Olli-Pekka Lehtokallio <53811784+oplekal@users.noreply.github.com>
2026-03-17 09:21:14 +0200

    EP-5000 (#1671)

--- eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/OsaAlueService.java
+++ eperusteet/eperusteet-service/src/main/java/fi/vm/sade/eperusteet/service/OsaAlueService.java
@@ -10,30 +10,30 @@ public interface OsaAlueService {
     @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'LUKU')")
     OsaAlueLaajaDto getOsaAlue(@P("viiteId") Long id, Long osaAlueId);
 
-    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS')")
+    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS') or hasPermission(#viiteId, 'tutkinnonosaviite', 'KORJAUS')")
     OsaAlueLaajaDto addOsaAlue(@P("viiteId") final Long viiteId, OsaAlueLaajaDto osaAlueDto);
 
-    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS')")
+    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS') or hasPermission(#viiteId, 'tutkinnonosaviite', 'KORJAUS')")
     OsaAlueLaajaDto addOsaAlue(Long perusteId, @P("viiteId") final Long viiteId, OsaAlueLaajaDto osaAlueDto);
 
-    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS')")
+    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS') or hasPermission(#viiteId, 'tutkinnonosaviite', 'KORJAUS')")
     OsaAlueLaajaDto updateOsaAlue(@P("viiteId") final Long viiteId, final Long osaAlueId, OsaAlueLaajaDto osaAlue);
 
-    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS')")
+    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS') or hasPermission(#viiteId, 'tutkinnonosaviite', 'KORJAUS')")
     OsaAlueLaajaDto updateOsaAlue(Long perusteId, @P("viiteId") final Long viiteId, final Long osaAlueId, OsaAlueLaajaDto osaAlue);
 
-    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS')")
+    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS') or hasPermission(#viiteId, 'tutkinnonosaviite', 'KORJAUS')")
     void removeOsaAlue(@P("viiteId") final Long viiteId, final Long osaAlueId);
 
-    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS')")
+    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS') or hasPermission(#viiteId, 'tutkinnonosaviite', 'KORJAUS')")
     void removeOsaAlue(Long perusteId, @P("viiteId") final Long viiteId, final Long osaAlueId);
 
-    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS')")
+    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'LUKU')")
     LukkoDto getOsaAlueLock(@P("viiteId") Long viiteId, Long osaAlueId);
 
-    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS')")
+    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS') or hasPermission(#viiteId, 'tutkinnonosaviite', 'KORJAUS')")
     LukkoDto lockOsaAlue(@P("viiteId") Long viiteId, Long osaAlueId);
 
-    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS')")
+    @PreAuthorize("hasPermission(#viiteId, 'tutkinnonosaviite', 'MUOKKAUS') or hasPermission(#viiteId, 'tutkinnonosaviite', 'KORJAUS')")
     void unlockOsaAlue(@P("viiteId") Long viiteId, Long osaAlueId);
 }
